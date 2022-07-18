584b0d352b1d2c121e85972f5118277d7d7f119b
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-07-18 22:15:33 +0530

    fix otp inputs toster not showing

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+// eslint-disable-next-line no-restricted-globals
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
 
 const OtpInput = ({ text, subText, boldText }) => {
-  var myopt = document.querySelectorAll(".input-field");
-  console.log(myopt);
-  for (var i = 0; i < myopt.length - 1; i++) {
-    myopt[i].addEventListener("keyup", function () {
-      this.nextElementSibling.focus();
+  const navigate = useNavigate();
+  const spinnerText = () => {
+    return (
+      <div className="flex items-center justify-start">
+        <Oval
+          ariaLabel="loading-indicator"
+          height={20}
+          width={20}
+          strokeWidth={5}
+          color="red"
+          secondaryColor="yellow"
+        />
+        <div className="ml-4 font-inter text-white text-16px ">
+          Verifying...
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    var myopt = document.querySelectorAll(".input-field");
+    console.log(myopt);
+    for (var i = 0; i < myopt.length - 1; i++) {
+      myopt[i].addEventListener("keyup", function () {
+        this.nextElementSibling.focus();
+      });
+    }
+  }, []);
+  function CheckOTP() {
+    console.log("next");
+    toast(spinnerText, {
+      position: "bottom-center",
+      autoClose: 1500,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      style: {
+        width: "200px",
+        borderRadius: "10%",
+        fontFamily: "Inter",
+        backgroundColor: "Green",
+      },
     });
+    setTimeout(() => {
+      navigate("/profile");
+    }, 2500);
   }
+  // console.log(i, myopt.length - 1);
+  // if (i === myopt.length - 1) {
+  //   console.log("enter");
+  //   myopt[myopt.length - 1].addEventListener("enter", function () {
+  //     console.log("enter");
+  //   });
+  // }
 
   return (
     <>
@@ -56,6 +110,7 @@ const OtpInput = ({ text, subText, boldText }) => {
                 type="text"
                 className="input-field"
                 maxLength={1}
+                onKeyPress={() => CheckOTP()}
               />
             </form>
           </div>
