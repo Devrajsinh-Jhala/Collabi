a8e733dc46a6befbe9b0f88c29a44cee706dc4b3
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-07-21 00:43:01 +0530

    Fixed overflow issues

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -13,17 +13,17 @@ const Header = () => {
         <div className="flex flex-col gap-12 items-center p-[10px] mt-[1rem]">
           <div className="flex  flex-col items-center gap-2 md:gap-4 lg:gap-6">
             <div className=" flex flex-wrap sm:w-[70%]  sm:justify-center ">
-              <div className="overflow-hidden mt-16">
-                <h1 className=" z-0 w-full max-w-[1000px] text-left sm:text-center big-text mb-[1.5rem] sm:mb-[0]  ">
-                  <span className="sm:p-2 px-5">
+              <div className="overflow-hidden pl-4 sm:pl-0 mt-16">
+                <h1 className=" z-0 w-full overflow-hidden max-w-[1000px] text-left sm:text-center big-text mb-[1.5rem] sm:mb-[0]  ">
+                  <span className="sm:p-2">
                     A professional networking platform built for
                   </span>
-                  <span className=" colored-text">developers</span>
+                  <span className=" p-2 sm:p-0 colored-text">developers</span>
                 </h1>
               </div>
             </div>
-            <div className="sm:mt-4  sm:w-[60%] sub-head overflow-hidden mb-[2rem] sm:mb-[0]">
-              <h1 className="text-center leading-[23px] sm:leading-[30px] tracking-[-0.014em] text-16px sm:text-[18px]">
+            <div className="sm:mt-4 pl-4 sm:pl-0 sm:w-[60%] sub-head overflow-hidden mb-[2rem] sm:mb-[0]">
+              <h1 className=" text-left sm:text-center leading-[23px] sm:leading-[30px] tracking-[-0.014em] text-16px sm:text-[18px]">
                 {" "}
                 This is where people who code connect, share their knowledge,
                 collaborate with like minded individuals and find new
