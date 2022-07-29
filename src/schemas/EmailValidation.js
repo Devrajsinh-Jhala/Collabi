2a6449b974ff3f668dfc895511bd3cfae4374712
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-07-29 22:39:02 +0530

    Modified error message in form validation

--- src/schemas/EmailValidation.js
+++ src/schemas/EmailValidation.js
@@ -1,8 +1,19 @@
 import * as yup from "yup";
+import { AiFillMinusCircle } from "react-icons/ai";
+
+const validationErrorMessage = (
+  <div className="flex items-center justify-start ">
+    <AiFillMinusCircle />
+    <p className="font-inter ml-1">Please enter a valid email address</p>
+  </div>
+);
+const emptyErrorMessage = (
+  <div className="flex items-center justify-start ">
+    <AiFillMinusCircle />
+    <p className="font-inter ml-1">Please enter an email address</p>
+  </div>
+);
 
 export const emailSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Please enter a valid email address")
-    .required("Please enter the email address"),
+  email: yup.string().email(validationErrorMessage).required(emptyErrorMessage),
 });
